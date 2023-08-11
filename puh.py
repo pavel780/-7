dd3065a266a076a38bb048c19225ce4ff57511e3
Pavel <pamuz@yandex.ru>
2023-08-11 14:19:28 +0300

    задача про винни пуха решена

--- puh.py
+++ puh.py
@@ -1,3 +1,19 @@
 def rythm(song):
     phrase=list(song.split(" "))
-    
+    a="а"
+    a_list=[]
+    flag=0
+    for i in phrase:
+        temp_a=0
+        for j in range(len(i)):
+            if i[j]=="а":
+                temp_a+=1
+        b=a_list.append(temp_a)
+        if b!=a_list[0]:
+            flag=1
+    if flag==1:
+        return "пам парам"
+    else:
+        return "парам пам-пам"
+s=input("введите фразу: ")
+print(rythm(s))
